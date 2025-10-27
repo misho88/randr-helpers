8b13bd4db2ca6c029afde2cbb4ccb5f3e1ea8c93
Mihail Georgiev <misho88@gmail.com>
2025-10-27 16:22:59 -0400

    randr-watch.c: await children

--- randr-watch.c
+++ randr-watch.c
@@ -5,18 +5,38 @@
 #include <xcb/randr.h>
 #include <unistd.h>
 #include <sys/wait.h>
+#include <signal.h>
 
-void
+pid_t
 spawn(char ** argv)
 {
 	pid_t pid = fork();
 	if (pid < 0) perror("randr-watch"), exit(1);
 	if (pid != 0) {
-		if (waitpid(pid, NULL, 0) < 0) perror("randr-watch"), exit(1);
-		return;
+		return pid;
 	};
 	execvp(argv[0], argv);
 	perror("randr-watch"), exit(1);
+}
+
+void
+await(pid_t pid, int sig)
+{
+	if (pid <= 0) {
+		return;
+	}
+	if (sig) {
+		if (kill(pid, sig) < 0) perror("randr-watch: kill()"), exit(1);
+	}
+	if (waitpid(pid, NULL, 0) < 0) perror("randr-watch: waitpid()"), exit(1);
+}
+
+pid_t current_pid = 0;
+
+void on_sigterm(int signum)
+{
+	await(current_pid, SIGTERM);
+	exit(0x80 | signum);
 }
 
 void
@@ -40,6 +60,11 @@ main(int argc, char ** argv)
 			usage();
 		}
 	}
+
+	struct sigaction sa = { .sa_handler = on_sigterm, .sa_flags = 0 };
+	sigemptyset(&sa.sa_mask);
+	if (sigaction(SIGTERM, &sa, NULL) < 0) perror("randr-watch: sigaction()"), exit(1);
+
 	xcb_connection_t * conn = xcb_connect(NULL, NULL);
 
 	xcb_screen_t * screen = xcb_setup_roots_iterator(xcb_get_setup(conn)).data;
@@ -47,9 +72,13 @@ main(int argc, char ** argv)
 	xcb_randr_select_input(conn, window, XCB_RANDR_NOTIFY_MASK_SCREEN_CHANGE);
 	xcb_flush(conn);
 
-	for (xcb_generic_event_t * event; (event = xcb_wait_for_event(conn)); free(event))
-		if (event->response_type & XCB_RANDR_NOTIFY_MASK_SCREEN_CHANGE)
-			spawn(argv);
+	for (xcb_generic_event_t * event; (event = xcb_wait_for_event(conn)); free(event)) {
+		if (event->response_type & XCB_RANDR_NOTIFY_MASK_SCREEN_CHANGE) {
+			await(current_pid, SIGTERM);
+			current_pid = spawn(argv);
+		}
+	}
+	await(current_pid, SIGTERM);
 	xcb_disconnect(conn);
 	return 0;
 }
